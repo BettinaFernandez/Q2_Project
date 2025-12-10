41fa1fae3b0f1f3d94f699a38ad9f4b9d4f19d12
BettinaFernadez <b2029bfbfernandez@pshs.edu.ph>
2025-12-10 10:14:57 +0800

    redirect everything omg

---
+++ webpages/backtoHOMEPAGE.js
@@ -0,0 +1,4 @@
+    function openTab(id) {
+    const desc = document.getElementById(id);
+    desc.classList.toggle("open");
+}
--- webpages/recipe_directing.js
+++ webpages/recipe_directing.js
@@ -1,13 +1,22 @@
 
     function redirect_recipe(name_of_recipe){
-        const quantity=prompt("How many servings? Pick from 1-5.");
 
         if (name_of_recipe=="beef_brocolli_recipe"){
+        const quantity=prompt("How many servings? Pick from 1-5.");
             if(quantity === "1"){
                 location.href="Beef_Brocolli/BF_OneServing.html";
             }
             else if(quantity === "2"){
                 location.href="Beef_Brocolli/BF_TwoServing.html";
+            }
+            else if(quantity === "3"){
+                location.href="Beef_Brocolli/BF_ThreeServing.html";
+            }
+            else if(quantity === "4"){
+                location.href="Beef_Brocolli/BF_FourServing.html";
+            }
+            else if(quantity === "5"){
+                location.href="Beef_Brocolli/BF_fiveServing.html";
             }
         }
 
