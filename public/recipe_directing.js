4f3827aa3b55c90980cbcc6e0783911a6283d436
BettinaFernadez <b2029bfbfernandez@pshs.edu.ph>
2025-12-11 18:07:11 +0800

    directing

--- public/recipe_directing.js
+++ public/recipe_directing.js
@@ -1,8 +1,9 @@
 
     function redirect_recipe(name_of_recipe){
+    const quantity=prompt("How many servings? Pick from 1-2.");
 
         if (name_of_recipe=="beef_brocolli_recipe"){
-        const quantity=prompt("How many servings? Pick from 1-5.");
+    const quantity=prompt("How many servings? Pick from 1-5.");
             if(quantity === "1"){
                 location.href="Beef_Brocolli/BF_OneServing.html";
             }
@@ -21,7 +22,52 @@
         }
 
         else if (name_of_recipe=="chicken_katsu_recipe"){
-                location.href="CK_recipes.html";
+            if(quantity === "1"){
+                location.href="chicken_recipes/CK_recipes.html";
+            }
+            else if(quantity === "2"){
+                location.href="chicken_recipes/BF_TwoServing.html";
+            }
+
+        }
+
+        else if (name_of_recipe=="crepes_recipe"){
+            if(quantity === "1"){
+                location.href="desserts_recipes/Crepes_Recipes_OneServing.html";
+            }
+            else if(quantity === "2"){
+                location.href="desserts_recipes/Crepes_Recipes_TwoServings.html";
+            }
+
+        }
+
+        else if (name_of_recipe=="horchata_recipe"){
+            if(quantity === "1"){
+                location.href="drinks_recipes/Horchata_OneServing.html";
+            }
+            else if(quantity === "2"){
+                location.href="drinks_recipes/Horchata_TwoServings.html";
+            }
+
+        }
+
+        else if (name_of_recipe=="GBSS_recipe"){
+            if(quantity === "1"){
+                location.href="pasta_recipes/GBSS_Recipes_OneServing.html";
+            }
+            else if(quantity === "2"){
+                location.href="pasta_recipes/GBSS_Recipes_TwoServings.html";
+            }
+
+        }
+
+        else if (name_of_recipe=="Pork_steak_recipe"){
+            if(quantity === "1"){
+                location.href="pork_recipes/PorkSteak_OneServing.html";
+            }
+            else if(quantity === "2"){
+                location.href="pork_recipes/PorkSteak_TwoServings.html";
+            }
 
         }
     }
